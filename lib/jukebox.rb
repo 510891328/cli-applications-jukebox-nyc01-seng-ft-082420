9a1e5cbab4b6cf166a20fb56bf063f903b764c53
Yunhao Gao <as510891328@gmail.com>
2020-08-21 21:08:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,13 +39,11 @@ def play(song)
       result = "playing #{v}"
     end
   }
-  binding.pry
   if result = ""
     puts "Invalid input, please try again"
   else
-    puts result
+    puts "#{result}"
   end
-  binding.pry
 end
 
 def exit_jukebox
